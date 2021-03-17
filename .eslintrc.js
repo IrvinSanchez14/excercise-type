fe1f97a0ae02e8df9840e08ebba13a3cc2eee64b
Irvin Sanchez <irvnsanchez@gmail.com>
2021-03-17 15:18:48 -0600

    second commit with change in package json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 const OFF = 'off'
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    '@ravn-dev/eslint-config-ravn/react',
-    '@ravn-dev/eslint-config-ravn/base',
-    '@ravn-dev/eslint-config-ravn/jest',
-    '@ravn-dev/eslint-config-ravn/typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
     project: './tsconfig.eslint.json',
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   rules: {
     '@typescript-eslint/no-non-null-assertion': OFF,
